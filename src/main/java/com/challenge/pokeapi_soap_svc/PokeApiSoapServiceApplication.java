0f9e57bf33d194dedc187124d162b9a99f891dea
Gerardo Amezquita <gerardo.amezquita@globant.com>
2024-07-11 22:08:07 -0600

    Initial Commit

---
+++ src/main/java/com/challenge/pokeapi_soap_svc/PokeApiSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.pokeapi_soap_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiSoapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/pokeapi_soap_svc/PokeApiSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.pokeapi_soap_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
